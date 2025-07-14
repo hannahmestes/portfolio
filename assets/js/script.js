380b34b8e2b34963d8d557e32c70c85645a6b66d
Hannah Estes <hannah@baltutech.com>
2025-07-14 01:26:19 -0700

    art page style update

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,7 +106,8 @@ for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
     for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page || navigationLinks[i].dataset.page === pages[i].dataset.page) {
         pages[i].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
